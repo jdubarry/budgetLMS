a6e31b322e490fcbaf003698f468c678f130fdd8
Bella <imyer@email.sc.edu>
2023-03-16 12:55:40 -0400

    another small push

--- Question.java
+++ Question.java
@@ -1,3 +1,7 @@
+/**
+ * This is going to create the questions for the quiz
+ * @author word.exe
+ */
 package budgetLMS;
 import java.util.ArrayList;
 
@@ -6,21 +10,49 @@ public class Question {
     private ArrayList<String> answerChoices;
     private int correctAnswer;
 
+    /**
+     * This is going to set up a question 
+     * @param title this is going to be the name of the question 
+     */
     public Question(String title) {
         
     }
+
+    /**
+     * This is going to remove certain answers from the questions
+     * @param index this is going to set up answers at a certain index
+     */
     public void removeAnswer(int index) {
 
     }
+
+    /**
+     * This is going to add an answer to a question
+     * @param answer The answer that is added to the question as an option
+     */
     public void addAnswer(String answer) {
         
     }
+
+    /**
+     * This is going to assign the answer to a certain location
+     * @param index this is going to be the location in the index where the answer is saved
+     * @param answer the answer that is added to the question as an option
+     */
     public void addAnswer(int index, String answer) {
 
     }
+
+    /**
+     * This will print the answer choices to each quiz question
+     */
     public void printChoices() {
 
     }
+
+    /**
+     * This is going to print the correct answer based on the question
+     */
     public void printCorrectAnswer() {
         
     }
