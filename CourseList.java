27d6a0b2d314b7719b9031ace3dfc887369eec3e
bmyer01 <bmyer01@outlook.com>
2023-03-21 12:55:24 -0400

    commit

--- CourseList.java
+++ CourseList.java
@@ -1,12 +1,27 @@
+/**
+ * This is going to set up the list of courses
+ * @author word.exe
+ */
 import java.util.ArrayList;
 
 public class CourseList {
     private ArrayList<Course> courses;
 
     private CourseList() {}
+
+    /**
+     * This is going to get the instance when the course is used
+     * @return the course list
+     */
     public ArrayList<Course> getInstance() {
         userList = new CourseList();
     }
+
+    /**
+     * This is going to allow for search when looking for courses by name
+     * @param keyword the keyword of the word looking up for the course name
+     * @return the name of the course
+     */
     public ArrayList<Course> SearchCoursesByName(String keyword) {
         Course temp = new Course();
         ArrayList<Course> courseList = new ArrayList<Course>();
@@ -18,6 +33,11 @@ public class CourseList {
         return temp;
     }
 
+    /**
+     * This is going to get the course by name
+     * @param keyword the keyword is the word looking up for the course name
+     * @return the name of the course
+     */
     public Course getCourseByName(String keyword) {
         Course temp = new Course();
         for(Course course: courses) {
@@ -28,6 +48,11 @@ public class CourseList {
         return temp;
     }
 
+    /**
+     * Gets the course by the ID
+     * @param courseID the course ID 
+     * @return the course based on the ID
+     */
     public Course getCourseByID(UUID courseID) {
         Course temp  = new Course();
         for(Course course: courses) {
@@ -38,18 +63,32 @@ public class CourseList {
         return temp;
     }
 
-
+    /**
+     * This is going to add in the courses
+     * @param course the course being used
+     */
     public void addCourse(Course course) {
         courses.add(course);
     }
+
+    /**
+     * This is going to delete the course
+     * @param course the course being used
+     */
     public void deleteCourse(Course course) {
         courses.remove(course);
     }
-
+    
+    /**
+     * This is going to read the courses from the JSON file
+     */
     public void readCoursesJSON() {
         DataWriter.readCourses(courses);
     }
 
+    /**
+     * This is going to save the courses
+     */
     public void save() {
         DataWriter.writeCourses(courses);
     }
