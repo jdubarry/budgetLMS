b4a5625f8fab22d1ad40fb97bb81cfb1d2bbfdb3
Jordan duBarry <jordandubarry73@gmail.com>
2023-03-29 10:47:34 -0400

    Added new author constructor

--- Author.java
+++ Author.java
@@ -7,6 +7,21 @@ import java.util.UUID;
 public class Author extends User{
 
     /**
+     * This class is going to set up a new author with all of their information
+     * @param firstName the first name of the user
+     * @param lastName the last name of the user
+     * @param userName the username for the user
+     * @param password the password for the user
+     * @param userID the UUID of the user
+     * @param dateOfBirth the date of birth for the user
+     * @param phoneNumber the phone number for the user
+     * @param emailAddress the email address for the user
+     */
+    public Author(String firstName, String lastName, String userName, String password, java.util.Date dateOfBirth, String phoneNumber, String emailAddress) {
+        super(firstName, lastName, userName, password, dateOfBirth, phoneNumber, emailAddress);
+    }
+
+    /**
      * This class is going to set up the author with all of their information
      * @param firstName the first name of the user
      * @param lastName the last name of the user
